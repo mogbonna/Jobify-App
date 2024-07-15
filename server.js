e2f3c911a627a1f69e33a3bc3d3ae12328c1c70e
mogbonna <mogbonna045@gmail.com>
2024-07-15 13:07:19 +0100

    Fixed front-end

--- server.js
+++ server.js
@@ -30,8 +30,7 @@ cloudinary.config({
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use(express.static(path.resolve(__dirname, "./public")));
-
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -44,7 +43,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 app.use("*", (req, res) => {
